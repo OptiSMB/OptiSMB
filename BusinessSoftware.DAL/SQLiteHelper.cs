c40e9abb8fda10f62fb9691a4a7c80bd9c2a52ba
Tanmay Seth <kushal.seth2010@gmail.com>
2024-11-19 20:06:32 +0530

    Created logic for security

---
+++ BusinessSoftware.DAL/SQLiteHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace BusinessSoftware.DAL
+{
+    public static class SQLiteHelper
+    {
+        public static IDbConnection GetConnection(string databaseName = "")
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            var _dbPath = GetProjectRoot(currentDirectory)+".DAL\\data\\";
+            if (string.IsNullOrWhiteSpace(_dbPath))
+                throw new InvalidOperationException("Database not initialized. Call Initialize() first.");
+
+            if (string.IsNullOrWhiteSpace(databaseName))
+                throw new InvalidOperationException("Database name is invalid.");
+
+            return new SqliteConnection($"Data Source={_dbPath+ databaseName};");
+        }
+
+        static string GetProjectRoot(string currentDirectory)
+        {
+            // Go up until we find the directory containing the .csproj file
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentDirectory);
+
+            while (directoryInfo != null && !File.Exists(Path.Combine(directoryInfo.FullName, $"{directoryInfo.Name}.csproj")))
+            {
+                directoryInfo = directoryInfo.Parent;
+            }
+
+            // Return the root directory (where the .csproj file is found)
+            return directoryInfo?.FullName;
+        }
+    }
+}
--- BusinessSoftware.Security/SecurityImpementation.cs
+++ BusinessSoftware.Security/SecurityImpementation.cs
@@ -1,10 +1,17 @@
-﻿namespace BusinessSoftware.Security
+﻿using BusinessSoftware.DAL;
+using Dapper;
+
+namespace BusinessSoftware.Security
 {
     public class SecurityImpementation : IBusinessSecurity
     {
         public void AuthenticateAndAuthorize()
         {
+            using var connection = SQLiteHelper.GetConnection("SecurityDB.db");
+            connection.Open();
 
+            var query = "SELECT * FROM User;";
+            var users =  connection.Query<User>(query);
         }
     }
 }
